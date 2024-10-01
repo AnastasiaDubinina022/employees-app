d41103134b833d7f48972b3d32656464ae832714
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-10-01 13:29:31 +0300

    Employees-list edited with using props key.

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,10 +4,13 @@ import './employees-list.css'
 
 const EmployeesList = ({data}) => {
 
-    const elements = data.map(item => {           // динамически формируем элементы на основе массива с объектами из бд
-
+    const elements = data.map(item => {      // динамически формируем элементы на основе массива с объектами из бд
         // return <EmployeesListItem name={item.name} salary={item.salary}/>
-        return <EmployeesListItem {...item}/>     // разворачиваем объект с помощью spread - получаем то же самое                                            
+        // return <EmployeesListItem {...item}/>     // разворачиваем объект с помощью spread - получаем то же самое 
+
+        const {id, ...itemProps} = item;     // выделяем id и остальные св-ва идут в itemProps - деструктуризация по отстаточному принципу
+
+        return <EmployeesListItem key={id} {...itemProps}/>  // записываем id в пропс key, а остальные пропсы идут дальше в itemProps                                         
     })
 
     return (
