21d44e353471c9748d87c4e65c8eff910757586f
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-10-11 17:29:14 +0300

    139. Props drilling and immutable.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,51 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]        
+        }
+    }
 
-    const data = [  // имитируем получение данных с базы
-        {name: 'John C.', salary: 800, increase: true, id: 1},
-        {name: 'Alex M.', salary: 3000, increase: false, id: 2},
-        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // 1 вариант:
+             // const index = data.findIndex(elem => elem.id === id);   // находим индекс элемента 
 
-    return (
-        <div className="app">
-            <AppInfo/>
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            // const newArr = [...before, ...after];
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>    
+            // 2 вариант:
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>    
+                </div>
+    
+                <EmployeesList 
+                data={this.state.data}      
+                onDelete={this.deleteItem}/>  
+                <EmployeesAddForm/>
             </div>
-
-            <EmployeesList data={data}/>   {/* прокидываем в компонент пропс с данными из базы для формирования списка */}
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css'
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {      // динамически формируем элементы на основе массива с объектами из бд
         // return <EmployeesListItem name={item.name} salary={item.salary}/>
@@ -10,7 +10,12 @@ const EmployeesList = ({data}) => {
 
         const {id, ...itemProps} = item;     // выделяем id и остальные св-ва идут в itemProps - деструктуризация по отстаточному принципу
 
-        return <EmployeesListItem key={id} {...itemProps}/>  // записываем id в пропс key, а остальные пропсы идут дальше в itemProps                                         
+        return (
+            <EmployeesListItem 
+                key={id} 
+                {...itemProps}  // записываем id в пропс key, а остальные пропсы идут дальше в itemProps
+                onDelete={() => onDelete(id)}/>                                           
+            )
     })
 
     return (
