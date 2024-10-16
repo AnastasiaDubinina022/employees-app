bd09eae7d606001efe156eab5f858e709d68f18a
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-10-16 15:23:52 +0300

    135. State lifting. Form-validation added to EmployeesAddForm.

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -7,7 +7,8 @@ class EmployeesAddForm extends Component {
         super(props);
         this.state = {
             name: '',
-            salary: ''
+            salary: '',
+            errorMessage: ''
         }
     }
 
@@ -19,16 +20,29 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+
         const {onAdd} = this.props;
-        onAdd(this.state.name, this.state.salary);   // при отправке формы передаем значения из стэйта в метод родителя
+        const {name, salary} = this.state;
+
+        // if (name.length < 3 || salary === '') return;   
+        if (name.length < 3 || !salary) {         // name короче 3х символов или salary не заполнен остановить отправку формы
+            this.setState({
+                errorMessage: 'Пожалуйста, заполните все поля.'  // и передаем сообщение в стейт
+            })
+            return;           
+        }
+        
+
+        onAdd(name, salary);   // при отправке формы передаем значения из стэйта в метод родителя
         this.setState({                            // очищаем стэйт (инпуты)
             name: '',
-            salary: ''
+            salary: '',
+            errorMessage: ''
         })
     }
 
     render() {
-        const {name, salary} = this.state;
+        const {name, salary, errorMessage} = this.state;
 
         return (
             <div className="app-add-form">
@@ -50,7 +64,12 @@ class EmployeesAddForm extends Component {
                         onChange={this.onValueChange} />
     
                     <button type="submit"
-                            className="btn btn-outline-light">Добавить</button>
+                            className="btn btn-outline-light"     // ниже блокировка кнопки если поля name и salary пустые
+                            disabled={!name || !salary}>Добавить</button>  
+                    
+                    {/* Условный рендеринг. Здесь {errorMessage && } проверяет значение в errorMessage и если да то рендерит блок с сообщением */}
+                    {errorMessage && <div className='error'>{errorMessage}</div>}  
+                   
                 </form>
             </div>
         );
